e4363d728ff09a02a343457cbb4c288968d33a9a
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2022-06-30 12:28:22 +0300

    ALL DONE!

--- src/jvmMain/kotlin/ru/spbstu/application/telegram/Strings.kt
+++ src/jvmMain/kotlin/ru/spbstu/application/telegram/Strings.kt
@@ -5,7 +5,7 @@ object Strings {
         "Привет, меня зовут UniStart, и я буду с тобой на протяжении всего, надеюсь, увлекательного и успешного пути по развитию собственной идеи до реального бизнеса! Для начала давай ты укажешь свой номер, чтобы получить доступ"
     const val SendPhoneButton = "Отправить номер телефона"
     const val Ideas = "Шаг Генерация идей"
-    const val Step1 = "GGG"
+    const val Step1 = "Шаг 1"
     const val Step2 = "Шаг 2"
     const val Step3 = "Шаг 3"
     const val Step4 = "Шаг 4"
@@ -13,4 +13,5 @@ object Strings {
     const val Button2 = "Метод Дельфи/брейншторм "
     const val Button3 = "SCAMPER"
     const val Button4 = "Trendy Friendy "
+    const val BBack = "Обратно к шагам"
 }
