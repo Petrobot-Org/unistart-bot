3430f8e5d202ab74717e42367c0dc6a4a1a7e1c2
LysenkoAleksandra <orange260302@gmail.com>
2022-07-13 02:00:20 +0300

    Get rid of waitTextFrom() from handlers which do not require the saving of state

--- server/src/main/kotlin/ru/spbstu/application/steps/telegram/StepFlow.kt
+++ server/src/main/kotlin/ru/spbstu/application/steps/telegram/StepFlow.kt
@@ -5,16 +5,13 @@ import dev.inmo.tgbotapi.extensions.behaviour_builder.BehaviourContext
 import dev.inmo.tgbotapi.extensions.utils.types.buttons.replyKeyboard
 import dev.inmo.tgbotapi.extensions.utils.types.buttons.row
 import dev.inmo.tgbotapi.extensions.utils.types.buttons.simpleButton
-import dev.inmo.tgbotapi.requests.send.SendTextMessage
 import dev.inmo.tgbotapi.types.message.abstracts.CommonMessage
 import dev.inmo.tgbotapi.types.message.content.TextContent
-import kotlinx.coroutines.flow.first
 import org.koin.core.context.GlobalContext
 import ru.spbstu.application.auth.entities.User
 import ru.spbstu.application.auth.repository.UserRepository
 import ru.spbstu.application.telegram.Strings
 import ru.spbstu.application.telegram.Strings.MyRanking
-import ru.spbstu.application.telegram.waitTextFrom
 
 private val userRepository: UserRepository by GlobalContext.get().inject()
 private val steps = listOf(Strings.Step1, Strings.Step2, Strings.Step3, Strings.Step4)
@@ -26,65 +23,46 @@ private val ideaGenerationMethods = listOf(
     Strings.BackToSteps
 )
 
-suspend fun BehaviourContext.steps(message: CommonMessage<TextContent>) {
+suspend fun BehaviourContext.handleSteps(message: CommonMessage<TextContent>) {
     val user = userRepository.get(User.Id(message.chat.id.chatId)) ?: return
-    val selectedStep = waitTextFrom(
-        message.chat,
-        SendTextMessage(
-            message.chat.id,
-            Strings.ChooseStep,
-            replyMarkup = replyKeyboard(
-                resizeKeyboard = true,
-                oneTimeKeyboard = true
-            )
-            {
-                row { simpleButton(Strings.GetMyStats) }
-                steps.take(user.availableStepsCount.toInt()).chunked(2).forEach {
-                    row {
-                        it.forEach { simpleButton(it) }
-                    }
+    sendTextMessage(
+        message.chat.id,
+        Strings.ChooseStep,
+        replyMarkup = replyKeyboard(
+            resizeKeyboard = true,
+            oneTimeKeyboard = true
+        )
+        {
+            row { simpleButton(Strings.GetMyStats) }
+            steps.take(user.availableStepsCount.toInt()).chunked(2).forEach {
+                row {
+                    it.forEach { simpleButton(it) }
                 }
             }
-        )
-    ).first { it.text in steps || it.text == Strings.GetMyStats }.text
-    when (selectedStep) {
-        Strings.Step1 -> handleStep1(message)
-        Strings.GetMyStats -> handleStats(message)
-    }
+        }
+    )
 }
 
 suspend fun BehaviourContext.handleStep1(message: CommonMessage<TextContent>) {
-    val stage = waitTextFrom(
-        message.chat,
-        SendTextMessage(
-            message.chat.id,
-            Strings.ChooseIdeaGeneration,
-            replyMarkup = replyKeyboard(
-                resizeKeyboard = true,
-                oneTimeKeyboard = true
-            ) {
+    sendTextMessage(
+        message.chat.id,
+        Strings.ChooseIdeaGeneration,
+        replyMarkup = replyKeyboard(
+            resizeKeyboard = true,
+            oneTimeKeyboard = true
+        ) {
+            ideaGenerationMethods.chunked(2).forEach {
                 row {
-                    simpleButton(Strings.Bisociation)
-                    simpleButton(Strings.DelphiBrainstormMethod)
-                }
-                row {
-                    simpleButton(Strings.Scamper)
-                    simpleButton(Strings.TrendyFriendy)
-                }
-                row {
-                    simpleButton(Strings.BackToSteps)
+                    it.forEach { simpleButton(it) }
                 }
             }
-        )
-    ).first { it.text in ideaGenerationMethods }.text
-    when (stage) {
-        Strings.BackToSteps -> steps(message)
-    }
+        }
+    )
 }
 
 suspend fun BehaviourContext.handleStats(message: CommonMessage<TextContent>) {
     val sortedUsers = userRepository.sortByAmountOfCoins()
     val user = userRepository.get(User.Id(message.chat.id.chatId)) ?: return
     sendTextMessage(message.chat.id, MyRanking(sortedUsers.size, sortedUsers.indexOf(user) + 1, user.amountOfCoins))
-    steps(message)
+    handleSteps(message)
 }
--- server/src/main/kotlin/ru/spbstu/application/telegram/TelegramBot.kt
+++ server/src/main/kotlin/ru/spbstu/application/telegram/TelegramBot.kt
@@ -3,12 +3,14 @@ package ru.spbstu.application.telegram
 import dev.inmo.tgbotapi.bot.ktor.telegramBot
 import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPolling
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
+import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onText
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import ru.spbstu.application.auth.telegram.handleStart
 import ru.spbstu.application.steps.telegram.handleStats
-import ru.spbstu.application.steps.telegram.steps
+import ru.spbstu.application.steps.telegram.handleStep1
+import ru.spbstu.application.steps.telegram.handleSteps
 
 class TelegramBot(token: TelegramToken) {
     private val bot = telegramBot(token.value)
@@ -18,8 +20,11 @@ class TelegramBot(token: TelegramToken) {
         coroutineScope.launch {
             bot.buildBehaviourWithLongPolling {
                 onCommand("start", scenarioReceiver = { handleStart(it) })
-                onCommand("steps", scenarioReceiver = { steps(it) })
-                onCommand("get_stats", scenarioReceiver = { handleStats(it) })
+                onCommand("steps", scenarioReceiver = { handleSteps(it) })
+                onCommand("stats", scenarioReceiver = { handleStats(it) })
+                onText({it.content.text == Strings.Step1}){ handleStep1(it)}
+                onText({it.content.text == Strings.GetMyStats}){ handleStats(it)}
+                onText({it.content.text ==  Strings.BackToSteps}){ handleSteps(it)}
             }.join()
         }
     }
