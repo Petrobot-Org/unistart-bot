9a9f168cc1df117921584143b542eb7a40e19e48
ithersta <schilyaev.vlad@gmail.com>
2022-07-13 17:14:24 +0300

    Add onSubscriberText

--- server/src/main/kotlin/ru/spbstu/application/telegram/TelegramBot.kt
+++ server/src/main/kotlin/ru/spbstu/application/telegram/TelegramBot.kt
@@ -3,12 +3,12 @@ package ru.spbstu.application.telegram
 import dev.inmo.tgbotapi.bot.ktor.telegramBot
 import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPolling
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
-import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onText
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import ru.spbstu.application.auth.telegram.handleStart
 import ru.spbstu.application.auth.telegram.onSubscriberCommand
+import ru.spbstu.application.auth.telegram.onSubscriberText
 import ru.spbstu.application.steps.telegram.handleStats
 import ru.spbstu.application.steps.telegram.handleStep1
 import ru.spbstu.application.steps.telegram.handleSteps
@@ -26,12 +26,10 @@ class TelegramBot(token: TelegramToken) {
                 onCommand("start") { handleStart(it) }
                 onSubscriberCommand("steps") { handleSteps(it) }
                 onSubscriberCommand("stats") { handleStats(it) }
-                onCommand("steps") { handleSteps(it) }
-                onCommand("stats") { handleStats(it) }
-                onText({ it.content.text in setOf(Strings.Step1, Strings.BackToIdeaGeneration) }) { handleStep1(it) }
-                onText({ it.content.text == Strings.GetMyStats }) { handleStats(it) }
-                onText({ it.content.text == Strings.BackToSteps }) { handleSteps(it) }
-                onText({ it.content.text == Strings.TrendyFriendy }) { sendTrendyFriendyApp(it.chat)}
+                onSubscriberText(Strings.Step1, Strings.BackToIdeaGeneration) { handleStep1(it) }
+                onSubscriberText(Strings.GetMyStats) { handleStats(it) }
+                onSubscriberText(Strings.BackToSteps) { handleSteps(it) }
+                onSubscriberText(Strings.TrendyFriendy) { sendTrendyFriendyApp(it.chat) }
             }.join()
         }
     }
