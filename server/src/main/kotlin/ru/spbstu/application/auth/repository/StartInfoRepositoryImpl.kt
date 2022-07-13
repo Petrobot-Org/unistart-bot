a9b767306577901cc085706e9a1481f2383398a5
LysenkoAleksandra <orange260302@gmail.com>
2022-07-13 22:14:03 +0300

    Ctrl+Alt+L

--- server/src/main/kotlin/ru/spbstu/application/auth/repository/StartInfoRepositoryImpl.kt
+++ server/src/main/kotlin/ru/spbstu/application/auth/repository/StartInfoRepositoryImpl.kt
@@ -5,15 +5,17 @@ import ru.spbstu.application.auth.entities.StartInfo
 import ru.spbstu.application.data.source.AppDatabase
 import java.time.Instant
 
-    private val mapper = { id: StartInfo.Id, number: PhoneNumber?, begin: Instant, end: Instant ->
-        StartInfo(id, number!!, begin, end)
-    }
-class StartInfoRepositoryImpl(private val database: AppDatabase) : StartInfoRepository  {
+private val mapper = { id: StartInfo.Id, number: PhoneNumber?, begin: Instant, end: Instant ->
+    StartInfo(id, number!!, begin, end)
+}
+
+class StartInfoRepositoryImpl(private val database: AppDatabase) : StartInfoRepository {
     override fun add(startInfo: StartInfo) {
         database.startInfoQueries.add(
             startInfo.phoneNumber,
             startInfo.begin,
-            startInfo.end)
+            startInfo.end
+        )
     }
 
     override fun contains(number: PhoneNumber): Boolean {
