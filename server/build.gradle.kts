8fd123c8dfb0f1819d95b91fae8227bb2943c20c
ithersta <schilyaev.vlad@gmail.com>
2022-07-26 20:02:45 +0300

    Move libraries to libs.versions.toml

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm")
-    kotlin("plugin.serialization")
-    id("com.squareup.sqldelight") version Versions.sqlDelight
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.sqldelight)
     application
 }
 
@@ -14,26 +14,15 @@ repositories {
 
 dependencies {
     implementation(project(":common"))
-    implementation("dev.inmo:tgbotapi:${Versions.telegramBotApi}")
-    implementation("io.ktor:ktor-server-netty:${Versions.ktor}")
-    implementation("io.ktor:ktor-server-html-builder-jvm:${Versions.ktor}")
-    implementation("io.ktor:ktor-server-auth:${Versions.ktor}")
-    implementation("io.ktor:ktor-server-content-negotiation:${Versions.ktor}")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:${Versions.ktor}")
-    implementation("io.ktor:ktor-server-compression:${Versions.ktor}")
-    implementation("io.ktor:ktor-server-status-pages:${Versions.ktor}")
-    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:${Versions.kotlinxHtml}")
-    implementation("io.insert-koin:koin-core:${Versions.koin}")
-    implementation("io.insert-koin:koin-ktor:${Versions.koin}")
-    implementation("io.insert-koin:koin-logger-slf4j:${Versions.koin}")
-    implementation("org.slf4j:slf4j-simple:${Versions.slf4jSimple}")
-    implementation("io.github.microutils:kotlin-logging-jvm:${Versions.kotlinLogging}")
-    implementation("com.squareup.sqldelight:sqlite-driver:${Versions.sqlDelight}")
-    implementation("com.squareup.sqldelight:coroutines-extensions-jvm:${Versions.sqlDelight}")
-    implementation("com.charleskorn.kaml:kaml:${Versions.kaml}")
-    implementation("org.apache.poi:poi:${Versions.poi}")
-    implementation("org.apache.poi:poi-ooxml:${Versions.poi}")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    implementation(libs.tgbotapi)
+    implementation(libs.bundles.ktor.server)
+    implementation(libs.kotlinx.html)
+    implementation(libs.bundles.koin)
+    implementation(libs.bundles.logging)
+    implementation(libs.sqldelight.sqlite)
+    implementation(libs.kaml)
+    implementation(libs.bundles.poi)
+    testImplementation(libs.junit.jupiter)
 }
 
 tasks.getByName<Test>("test") {
